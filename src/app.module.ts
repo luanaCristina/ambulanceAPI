35e1dc137b8ccefdbb425b246ace8f1e6de24937
Luana Cristina de Albuquerque Silva <luanacrystinne@msn.com>
2024-09-01 06:38:22 -0300

    realizando o deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,12 @@ import { AmbulanceModule } from './ambulance/ambulance.module';
     ConfigModule.forRoot(),
         TypeOrmModule.forRoot({
           type: 'postgres',
-          host: process.env.DB_HOST,
-          port: parseInt(process.env.DB_PORT),
-          username: process.env.DB_USERNAME,
-          password: process.env.DB_PASSWORD,
-          database: process.env.DB_NAME,
+          url: process.env.DATABASE_URL,
+          // host: process.env.DB_HOST,
+          // port: parseInt(process.env.DB_PORT),
+          // username: process.env.DB_USERNAME,
+          // password: process.env.DB_PASSWORD,
+          // database: process.env.DB_NAME,
           entities: [
             __dirname + '/**/*.entity{.ts,.js}',
           ],
